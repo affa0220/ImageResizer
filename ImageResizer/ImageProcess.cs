97c0e417fcb9b8e8819fbe1f33cb90dff8a80978
affa0220 <affa0220@gmail.com>
2019-10-02 13:51:27 +0800

    modify the synchronous funtion to asynchronous
    modify the synchronous funtion to asynchronous

--- ImageResizer/ImageProcess.cs
+++ ImageResizer/ImageProcess.cs
@@ -3,6 +3,7 @@ using System.Drawing;
 using System.Drawing.Drawing2D;
 using System.Drawing.Imaging;
 using System.IO;
+using System.Threading.Tasks;
 
 namespace ImageResizer
 {
@@ -30,6 +31,113 @@ namespace ImageResizer
         }
 
         /// <summary>
+        /// 找出指定目錄下的圖片
+        /// </summary>
+        /// <param name="srcPath">圖片來源目錄路徑</param>
+        /// <returns></returns>
+        public List<string> FindImages(string srcPath)
+        {
+            string[] fileExtentionNames = { "*.png", "*.jpg", "*.jpeg" };
+            var files = new List<string>();
+
+            foreach (var names in fileExtentionNames)
+            {
+                files.AddRange(Directory.GetFiles(srcPath, names, SearchOption.AllDirectories));
+            }
+
+            return files;
+        }
+
+        /// <summary>
+        /// 找出指定目錄下的圖片
+        /// </summary>
+        /// <param name="srcPath">圖片來源目錄路徑</param>
+        /// <returns></returns>
+        public List<string> FindImagesByName(string srcPath, string name)
+        {
+            var files = new List<string>();
+            files.AddRange(Directory.GetFiles(srcPath, name, SearchOption.AllDirectories));
+
+            return files;
+        }
+
+        #region 非同步
+
+        /// <summary>
+        /// 進行圖片的縮放作業
+        /// </summary>
+        /// <param name="sourcePath">圖片來源目錄路徑</param>
+        /// <param name="destPath">產生圖片目的目錄路徑</param>
+        /// <param name="scale">縮放比例</param>
+        public async Task ResizeImagesAsync(string sourcePath, string destPath, double scale)
+        {
+            string[] fileNames = { "*.png", "*.jpg", "*.jpeg" };
+
+            var jobs = new List<Task>();
+            foreach (var name in fileNames)
+            {
+                var files = FindImagesByName(sourcePath, name);
+                jobs.Add(Task.Run(async () =>
+                {
+                    var tasks = new List<Task>();
+                    foreach (var filePath in files)
+                    {
+                        Image imgPhoto = Image.FromFile(filePath);
+                        string imgName = Path.GetFileNameWithoutExtension(filePath);
+
+                        int sourceWidth = imgPhoto.Width;
+                        int sourceHeight = imgPhoto.Height;
+
+                        int destionatonWidth = (int)(sourceWidth * scale);
+                        int destionatonHeight = (int)(sourceHeight * scale);
+
+                        tasks.Add(Task.Run(async () =>
+                        {
+                            Bitmap processedImage = await ProcessBitmapAsync((Bitmap)imgPhoto,
+                            sourceWidth, sourceHeight,
+                            destionatonWidth, destionatonHeight);
+
+                            string destFile = Path.Combine(destPath, imgName + ".jpg");
+                            processedImage.Save(destFile, ImageFormat.Jpeg);
+                        }));
+                    }
+                    await Task.WhenAll(tasks);
+                }));
+                
+            }
+            await Task.WhenAll(jobs);
+        }
+
+        /// <summary>
+        /// 針對指定圖片進行縮放作業
+        /// </summary>
+        /// <param name="img">圖片來源</param>
+        /// <param name="srcWidth">原始寬度</param>
+        /// <param name="srcHeight">原始高度</param>
+        /// <param name="newWidth">新圖片的寬度</param>
+        /// <param name="newHeight">新圖片的高度</param>
+        /// <returns></returns>
+        private async Task<Bitmap> ProcessBitmapAsync(Bitmap img, int srcWidth, int srcHeight, int newWidth, int newHeight)
+        {
+            return await Task.Run(() =>
+            {
+                Bitmap resizedbitmap = new Bitmap(newWidth, newHeight);
+                Graphics g = Graphics.FromImage(resizedbitmap);
+                g.InterpolationMode = InterpolationMode.High;
+                g.SmoothingMode = SmoothingMode.HighQuality;
+                g.Clear(Color.Transparent);
+                g.DrawImage(img,
+                    new Rectangle(0, 0, newWidth, newHeight),
+                    new Rectangle(0, 0, srcWidth, srcHeight),
+                    GraphicsUnit.Pixel);
+                return resizedbitmap;
+            });
+        }
+
+        #endregion
+
+        #region 同步
+        /// <summary>
         /// 進行圖片的縮放作業
         /// </summary>
         /// <param name="sourcePath">圖片來源目錄路徑</param>
@@ -49,27 +157,13 @@ namespace ImageResizer
                 int destionatonWidth = (int)(sourceWidth * scale);
                 int destionatonHeight = (int)(sourceHeight * scale);
 
-                Bitmap processedImage = processBitmap((Bitmap)imgPhoto,
+                Bitmap processedImage = ProcessBitmap((Bitmap)imgPhoto,
                     sourceWidth, sourceHeight,
                     destionatonWidth, destionatonHeight);
 
                 string destFile = Path.Combine(destPath, imgName + ".jpg");
                 processedImage.Save(destFile, ImageFormat.Jpeg);
             }
-        }
-
-        /// <summary>
-        /// 找出指定目錄下的圖片
-        /// </summary>
-        /// <param name="srcPath">圖片來源目錄路徑</param>
-        /// <returns></returns>
-        public List<string> FindImages(string srcPath)
-        {
-            List<string> files = new List<string>();
-            files.AddRange(Directory.GetFiles(srcPath, "*.png", SearchOption.AllDirectories));
-            files.AddRange(Directory.GetFiles(srcPath, "*.jpg", SearchOption.AllDirectories));
-            files.AddRange(Directory.GetFiles(srcPath, "*.jpeg", SearchOption.AllDirectories));
-            return files;
         }
 
         /// <summary>
@@ -81,7 +175,7 @@ namespace ImageResizer
         /// <param name="newWidth">新圖片的寬度</param>
         /// <param name="newHeight">新圖片的高度</param>
         /// <returns></returns>
-        Bitmap processBitmap(Bitmap img, int srcWidth, int srcHeight, int newWidth, int newHeight)
+        Bitmap ProcessBitmap(Bitmap img, int srcWidth, int srcHeight, int newWidth, int newHeight)
         {
             Bitmap resizedbitmap = new Bitmap(newWidth, newHeight);
             Graphics g = Graphics.FromImage(resizedbitmap);
@@ -94,5 +188,7 @@ namespace ImageResizer
                 GraphicsUnit.Pixel);
             return resizedbitmap;
         }
+
+        #endregion
     }
 }
