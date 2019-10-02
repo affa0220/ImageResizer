71d0bef26af86b911d81ebe1cd504e133c15d690
affa0220 <affa0220@gmail.com>
2019-10-02 13:51:27 +0800

    modify the synchronous funtion to asynchronous
    modify the synchronous funtion to asynchronous

--- ImageResizer/Program.cs
+++ ImageResizer/Program.cs
@@ -9,10 +9,10 @@ namespace ImageResizer
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
             string sourcePath = Path.Combine(Environment.CurrentDirectory, "images");
-            string destinationPath = Path.Combine(Environment.CurrentDirectory, "output"); ;
+            string destinationPath = Path.Combine(Environment.CurrentDirectory, "output");
 
             ImageProcess imageProcess = new ImageProcess();
 
@@ -22,8 +22,24 @@ namespace ImageResizer
             sw.Start();
             imageProcess.ResizeImages(sourcePath, destinationPath, 2.0);
             sw.Stop();
+            var oriTime = sw.ElapsedMilliseconds;
+            imageProcess.Clean(destinationPath);
 
-            Console.WriteLine($"花費時間: {sw.ElapsedMilliseconds} ms");
+            Console.WriteLine($"同步-花費時間: {oriTime} ms");
+
+            sw.Restart();
+            await imageProcess.ResizeImagesAsync(sourcePath, destinationPath, 2.0);
+            sw.Stop();
+            var asyncTime = sw.ElapsedMilliseconds;
+            Console.WriteLine($"非同步-花費時間: {asyncTime} ms");
+
+            var improve = ((float)(oriTime - asyncTime) / oriTime) * 100;
+
+            Console.WriteLine($"{improve}%");
+
+            Console.ReadKey();
+
+
         }
     }
 }
